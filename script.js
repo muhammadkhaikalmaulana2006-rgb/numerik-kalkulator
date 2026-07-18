6fd50cc13dc9aad362e82a20c08c8f83023aee5a
muhammadkhaikalmaulana2006-rgb <muhammadkhaikalmaulana2006@gmail.com>
2026-07-18 14:45:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -531,3 +531,32 @@ function resetGauss(){
         chartGauss = null;
     }
 }
+
+function login(){
+
+    let username =
+    document.getElementById(
+    "username").value;
+
+    let password =
+    document.getElementById(
+    "password").value;
+
+    if(
+        username=="admin" &&
+        password=="admin"
+    ){
+
+        window.location.href =
+        "dashboard.html";
+
+    }
+
+    else{
+
+        alert(
+        "Username atau Password salah!"
+        );
+
+    }
+}
